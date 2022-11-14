1ea2c6f5db2b9c7632223339a8bb17867b82f815
Malick Ouedraogo <C00400598@louisiana.edu>
2022-11-13 23:27:43 -0600

    Display api data in a table in homepage

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
+import './Home.css';
 function Home() {
 
     const [searchInput, setSearchInput] = useState("");
@@ -7,45 +8,51 @@ function Home() {
     const handleChange = (e: any) => {
         setSearchInput(e.target.value);
         
-    }    
-    // const titles: string[] = ;
-    
+    } 
     const fetchResults = async () => {
         await fetch(`https://api.jikan.moe/v4/anime?q=${searchInput}`)
         .then(res => res.json())
         .then((result) => {
-            setAnimeData(result.data);
-            console.log(animeData); 
-            });
+            setAnimeData(result.data);             
+        }).catch((err) => {
+            console.log(err);
+            
+        });
     }
 
   return (
-    <div className="">
+    <div className="container">
         <h2 className="text-center">Anime Search</h2>
         <div className="form-group">
-          <i className="fa fa-search"/>
           <input type="text" value={searchInput} onChange={handleChange} />
           <button onClick={fetchResults}>Search</button>
         </div>
-    
-
-<table className="table" >
-    <thead className='thead-dark'>
-        <tr>
-            <th>#</th>
-            <th colSpan={2}>Title</th>
-        </tr>
-    </thead>
-    <tbody>
-            {
-                animeData.map((entry: any, key: number) => (
+        <table className="table" >
+            <thead className='thead-dark'>
                 <tr>
-                    <th>{key}</th>
-                    <td key={key}>Title: {entry.title}</td>
-                </tr>))
-            }
-    </tbody>
-</table>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Image</th>
+                    <th>Episode(s)</th>
+                    <th>Status</th>
+                    <th>Type</th>
+                </tr>
+            </thead>
+            <tbody>
+                    {
+                        animeData.map((entry: any, key: number) => (
+                        <tr key={key}>
+                            <th>{key}</th>
+                            <td>{entry.title}</td>
+                            <td><img className="my-images" src={entry.images.jpg.image_url} alt="none" /></td>
+                            <td>{entry.episodes}</td>
+                            <td>{entry.status}</td>
+                            <td>{entry.type}</td>
+
+                        </tr>))
+                    }
+            </tbody>
+        </table>
 
     </div>
   )
