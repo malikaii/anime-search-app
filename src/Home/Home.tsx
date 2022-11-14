8a6e2285dc22ceaa38c2bbd3a69c73e41fbee663
Malick Ouedraogo <C00400598@louisiana.edu>
2022-11-13 22:52:45 -0600

    Implemented search feature using ajax and jikan api

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
-
+import {useState, useEffect} from 'react';
 function Home() {
+
+    const [searchInput, setSearchInput] = useState("");
+    const [animeData, setAnimeData]: any = useState([]);
+    const handleChange = (e: any) => {
+        setSearchInput(e.target.value);
+        
+    }    
+    // const titles: string[] = ;
+    
+    const fetchResults = async () => {
+        await fetch(`https://api.jikan.moe/v4/anime?q=${searchInput}`)
+        .then(res => res.json())
+        .then((result) => {
+            setAnimeData(result.data);
+            console.log(animeData); 
+            });
+    }
+
   return (
-    <div>Home</div>
+    <div className="">
+        <h2 className="text-center">Anime Search</h2>
+        <div className="form-group">
+          <i className="fa fa-search"/>
+          <input type="text" value={searchInput} onChange={handleChange} />
+          <button onClick={fetchResults}>Search</button>
+        </div>
+    
+
+<table className="table" >
+    <thead className='thead-dark'>
+        <tr>
+            <th>#</th>
+            <th colSpan={2}>Title</th>
+        </tr>
+    </thead>
+    <tbody>
+            {
+                animeData.map((entry: any, key: number) => (
+                <tr>
+                    <th>{key}</th>
+                    <td key={key}>Title: {entry.title}</td>
+                </tr>))
+            }
+    </tbody>
+</table>
+
+    </div>
   )
 }
 
